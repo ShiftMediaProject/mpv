ab91003f61b84530cfefe31cc6c27b12216d83d1
Matt Oliver <protogonoi@gmail.com>
2018-02-18 19:41:45 +1100

    project: Update for 0.28.02 merge.

--- SMP/version.h
+++ SMP/version.h
@@ -1,3 +1,3 @@
-#define VERSION "0.28.0-113-ga5f53da"
-#define BUILDDATE "Sun Jan 14 20:05:50 DST 2018"
+#define VERSION "4a59705-dirty"
+#define BUILDDATE "Sun Feb 18 19:39:25 DST 2018"
 #define MPVCOPYRIGHT "Copyright © 2000-2018 mpv/MPlayer/mplayer2 projects"
